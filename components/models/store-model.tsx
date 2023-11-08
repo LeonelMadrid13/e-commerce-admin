93f8dc7434a2f54f0f483bd0f1843d8cb5e3ea6d
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-11-08 17:10:56 -0600

    feat: Store form creation

--- components/models/store-model.tsx
+++ components/models/store-model.tsx
@@ -1,10 +1,34 @@
 'use client'
 
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 import { Model } from '@/components/ui/model'
 import { useStoreModel } from '@/hooks/use-store-model'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const formSchema = z.object({
+  name: z.string().min(1)
+})
 
 export const StoreModel = () => {
   const storeModel = useStoreModel()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: ''
+    }
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+    // TODO: create store
+  }
+
   return (
         <Model
         title='Create Store'
@@ -12,7 +36,31 @@ export const StoreModel = () => {
         isOpen={storeModel.isOpen}
         onClose={storeModel.onClose}
     >
-        Future create store form
+      <div>
+        <div className='space-y-4 py-2 pb-4'>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Ecommerce' {...field}/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className='pt-6 space-x-6 flex items-center justify-end w-full'>
+                <Button variant='outline' onClick={storeModel.onClose} >Cancel</Button>
+                <Button type='submit' >Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
     </Model>
   )
 }
