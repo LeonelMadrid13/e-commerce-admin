65d58ce7cd274be43d1cd7a641343a5810a939c1
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-11-07 21:22:59 -0600

    feat: store creation form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,19 @@
 'use client'
 
+import { useEffect } from 'react'
+
+import { useStoreModel } from '@/hooks/use-store-model'
+
 const SetupPage = () => {
+  const onOpen = useStoreModel((state) => state.onOpen)
+  const isOpen = useStoreModel((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
         <div className='p-4'>
           Root Page
